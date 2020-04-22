07eedb337770adfbf674e863274d01fa9ca5d9ff
Robin Eschbach <LADBukkit@gmail.com>
2020-04-22 02:06:51 +0200

    removed cacert

--- src/erlsmtp_serv.erl
+++ src/erlsmtp_serv.erl
@@ -101,7 +101,7 @@ handle_info(?SOCK("EHLO "++Str), S) ->
 handle_info(?SOCK("STARTTLS"++_), S = #state{socket=Socket, helo=ehlo}) ->
     starttls(S),
     ok = inet:setopts(Socket, [{active, false}]),
-    {ok, SslSocket} = ssl:ssl_accept(Socket, [{cacertfile, "cacerts.pem"},{certfile, "cert.pem"}, {keyfile, "key.pem"}]),
+    {ok, SslSocket} = ssl:ssl_accept(Socket, [{certfile, "cert.pem"}, {keyfile, "key.pem"}]),
     {noreply, S#state{socket=SslSocket,type=ssl}};
 
 %% Handle MAIL FROM
